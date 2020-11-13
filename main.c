924b69f47acc13b5f5d2911de2ad59912c79c880
SelmanB <selmanbursal0@gmail.com>
2020-11-13 09:53:37 -0500

    week1.5part2 com error?
    got a really basic text base gui working but having error with rs232 part may be missing stuff on my end

--- main.c
+++ main.c
@@ -28,7 +28,7 @@ int main(int argc, char* argv[]) {
 
 	}
 	if (save=='c') {
-		return(0);
+		mainrs232();
 
 
 	}
--- mainrs232.c
+++ mainrs232.c
@@ -9,10 +9,10 @@
 #include <string.h>
 #include "RS232Comm.h"
 
-
+#define BUFSIZE 140
 
 // Declare constants, variables and communication parameters
-const int BUFSIZE = 140;							// Buffer size
+//const int BUFSIZE = 140;							// Buffer size
 //setting up two virtual ports
 wchar_t COMPORT_Rx[] = L"COM7";						// COM port used for Rx (use L"COM6" for transmit program)
 wchar_t COMPORT_Tx[] = L"COM6";						// COM port used for Rx (use L"COM6" for transmit program)
@@ -27,26 +27,6 @@ COMMTIMEOUTS timeout;								// A commtimeout struct variable
 
 int mainrs232() {
 
-	// Note: Comment out the Tx or Rx sections below to operate in single sided mode
-
-	// Declare constants, variables and communication parameters
-	const int BUFSIZE = 140;							// Buffer size
-	//setting up two virtual ports
-	wchar_t COMPORT_Rx[] = L"COM7";						// COM port used for Rx (use L"COM6" for transmit program)
-	wchar_t COMPORT_Tx[] = L"COM6";						// COM port used for Rx (use L"COM6" for transmit program)
-
-	// Communication variables and parameters
-	HANDLE hComRx;										// Pointer to the selected COM port (Receiver)
-	HANDLE hComTx;										// Pointer to the selected COM port (Transmitter)
-	int nComRate = 9600;								// Baud (Bit) rate in bits/second 
-	int nComBits = 8;									// Number of bits per frame
-	COMMTIMEOUTS timeout;								// A commtimeout struct variable
-
-}
-
-/*
- // The client - A testing main that calls the functions below
-int main() {
 
 	// Set up both sides of the comm link
 	initPort(&hComRx, COMPORT_Rx, nComRate, nComBits, timeout);	// Initialize the Rx port
@@ -76,4 +56,3 @@ int main() {
 	system("pause");
 }
 
-*/
